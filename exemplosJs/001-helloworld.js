08e0b6d88ba638ecaf17f876735e66db91151c1e
brurodri712 <brunorodrigues.p54@gmail.com>
2023-02-13 22:48:47 -0300

    Aula - Introdução
    Criação de variáveis, concatenação e tipagens.

---
+++ exemplosJs/001-helloworld.js
@@ -0,0 +1,36 @@
+// comentário de uma linha
+console.log(" Estou desenvolvendo em JavaScript ");
+
+/* 
+    comentário de
+    mais de 1 linha
+    Três formas de declarar variável em Javascript 
+    (var, let, const)
+*/
+
+let nome = 'Bruno';
+let idade = 24;
+let trabalha = false;
+let alturaPessoa = 1.75;  
+let coresFavoritas = ['roxo', 'preto', 'laranja', 'verde']; //array
+let endereco = {
+    logradouro : 'Rua',
+    nomeLog : 'Tapajós',
+    num : '217',
+    bairro : 'Centro'
+}; //objeto
+let covid = null;
+let dengue;
+
+console.log(alturaPessoa);
+console.log('Nome: ' + nome);
+console.log('O ' + nome + ' tem ' + idade + ' anos. ' );
+console.log('');;
+console.log('----------------------------------');
+console.log(typeof(nome)); //Descobrir tipo da variável
+console.log(typeof(idade)); //Descobrir tipo da variável
+console.log(typeof(coresFavoritas)); //Descobrir tipo da variável
+console.log(typeof(dengue)); //Descobrir tipo da variável
+
+
+
